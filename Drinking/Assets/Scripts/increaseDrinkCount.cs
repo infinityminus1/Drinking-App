685e51f4f99e6ea5e63ab03e31b2d41cc6ff7729
Kurt Lang <kurtalang@gmail.com>
2015-10-13 13:51:43 -0700

    Added 1 button, button image, text and script. Increments beer counter

---
+++ Drinking/Assets/Scripts/increaseDrinkCount.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class increaseDrinkCount : MonoBehaviour {
+
+	public Text drinkText;
+
+	private int drinkCount;
+	
+
+	void Awake ()
+	{
+		drinkText = GetComponent<Text> ();
+		setCountText ();
+		drinkCount = 0;
+	}
+ 
+	public void drinkUp () 
+	{
+		drinkCount++;
+		setCountText();
+	}
+
+	void setCountText ()
+	{
+		drinkText.text = "You've drank " + drinkCount.ToString () + " beers!";
+	}
+}
